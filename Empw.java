90dd3d8ece80eb486a9bb0ca6af58627dd6dc9bf
tejasdcr7 <tejasdandge@gmail.com>
2021-12-10 12:46:15 +0530

    added part time

--- Empw.java
+++ Empw.java
@@ -7,25 +7,33 @@ import java.util.Scanner;
 
     class EmpWage{
 
-            public static void main (String[]args){
-                int wageperhr=20,fulldayhr=8;
-            Scanner input = new Scanner( System.in);
-            Math math = null;
-            int option = (int) (math.random() * 3);
-            switch (option) {
-                case 1:
-                    System.out.println("present");
-                    System.out.println(wageperhr*fulldayhr);
-                    break;
-                case 2:
-                    System.out.println("absent");
-                    break;
-                default:
-                    System.out.println("no case");
+            public static void main (String[]args) {
+                int wageperhour = 20, fulldayhour, pay;
+                Scanner input = new Scanner(System.in);
+                Math math = null;
+                int option = (int) (math.random() * 3);
+                switch (option) {
+                    case 1:
+                        System.out.println("present");
+                        System.out.print("Enter how many hours worked: ");
+                        int hours = input.nextInt();
+                        if (hours > 8) {
+                            System.out.println(wageperhour * hours);
+                        } else {
+                            System.out.println(hours * wageperhour);
+
+                        }
+                        System.out.println();
+                        break;
+                    case 2:
+                        System.out.println("absent");
+                        break;
+                    default:
+                        System.out.println("no case");}
+                }
             }
-        }
 
 
-    }
+
 
 
