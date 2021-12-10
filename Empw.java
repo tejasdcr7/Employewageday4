62abe9f74f8a624bae0ae56e5dfeb6b2332073f2
tejasdcr7 <tejasdandge@gmail.com>
2021-12-10 13:16:50 +0530

    month salary

--- Empw.java
+++ Empw.java
@@ -8,22 +8,29 @@ import java.util.Scanner;
     class EmpWage{
 
             public static void main (String[]args) {
-                int wageperhour = 20, fulldayhour, pay;
+                int i,wageperhour = 20, fulldayhour, pay,total=0,salary=0;
                 Scanner input = new Scanner(System.in);
                 Math math = null;
                 int option = (int) (math.random() * 3);
-                switch (option) {
+
+               switch (option) {
                     case 1:
                         System.out.println("present");
-                        System.out.print("Enter how many hours worked: ");
-                        int hours = input.nextInt();
-                        if (hours > 8) {
-                            System.out.println(wageperhour * hours);
-                        } else {
-                            System.out.println(hours * wageperhour);
+                        for(i=1;i<20;i++) {
+                            System.out.print("Enter how many hours worked: ");
+                            int hours = input.nextInt();
+                            if (hours > 8) {
+                               total=wageperhour * hours;
+                               salary=salary+total; //total calculation  value stored in salary
+                                System.out.println(salary);
 
+                            } else {total=wageperhour * hours;
+                                salary=salary+total;
+                                System.out.println(salary);
+
+                            }
+                            System.out.println();
                         }
-                        System.out.println();
                         break;
                     case 2:
                         System.out.println("absent");
