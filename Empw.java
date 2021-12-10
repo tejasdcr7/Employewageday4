8afadd80386a9b6bc18b303fe4b3c3d1af0494ab
tejasdcr7 <tejasdandge@gmail.com>
2021-12-10 12:12:53 +0530

    daily wages ad

--- Empw.java
+++ Empw.java
@@ -1,2 +1,31 @@
-package com.assingment.dayfour;public class Empw {
-}
+package com.assingment.dayfour;
+
+import java.util.Scanner;
+
+
+
+
+    class EmpWage{
+
+            public static void main (String[]args){
+                int wageperhr=20,fulldayhr=8;
+            Scanner input = new Scanner( System.in);
+            Math math = null;
+            int option = (int) (math.random() * 3);
+            switch (option) {
+                case 1:
+                    System.out.println("present");
+                    System.out.println(wageperhr*fulldayhr);
+                    break;
+                case 2:
+                    System.out.println("absent");
+                    break;
+                default:
+                    System.out.println("no case");
+            }
+        }
+
+
+    }
+
+
