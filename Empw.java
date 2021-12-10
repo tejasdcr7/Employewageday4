4c16ea6eef68c7ca219de34f88d0f50455415ded
tejasdcr7 <tejasdandge@gmail.com>
2021-12-10 13:36:14 +0530

    total hours

--- Empw.java
+++ Empw.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
     class EmpWage{
 
             public static void main (String[]args) {
-                int i,wageperhour = 20, fulldayhour, pay,total=0,salary=0;
+                int i,wageperhour = 20, fulldayhour, pay,total=0,salary=0,totalhours=100;
                 Scanner input = new Scanner(System.in);
                 Math math = null;
                 int option = (int) (math.random() * 3);
@@ -16,16 +16,20 @@ import java.util.Scanner;
                switch (option) {
                     case 1:
                         System.out.println("present");
-                        for(i=1;i<20;i++) {
+                        for(i=1;i<totalhours;i++) {
                             System.out.print("Enter how many hours worked: ");
                             int hours = input.nextInt();
                             if (hours > 8) {
                                total=wageperhour * hours;
                                salary=salary+total; //total calculation  value stored in salary
+                                total=wageperhour * hours;
+                                totalhours=totalhours - hours; //previous value of totalhour saved in totolhours
                                 System.out.println(salary);
 
                             } else {total=wageperhour * hours;
                                 salary=salary+total;
+                                total=wageperhour * hours;
+                                totalhours= totalhours - hours;
                                 System.out.println(salary);
 
                             }
